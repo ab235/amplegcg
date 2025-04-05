e6340fb7cc0fff1fa74c0a0aa01a10d97a8a5f9d
CharlieC133 <69218910+CharlieC133@users.noreply.github.com>
2025-04-05 01:25:39 -0400

    Update run_overgenerate_indiv_query_indiv_model_llama2-chat.sh

--- llmattack/experiments/launch_scripts/run_overgenerate_indiv_query_indiv_model_llama2-chat.sh
+++ llmattack/experiments/launch_scripts/run_overgenerate_indiv_query_indiv_model_llama2-chat.sh
@@ -3,7 +3,7 @@ set -e
 
 
 n_steps=100
-for offset in $(seq 0)
+for offset in $(seq 0 10 100)
 do
     bash run_overgenerate_indiv_query_indiv_model.sh indiv_model_llama2-chat $offset $n_steps
 done
