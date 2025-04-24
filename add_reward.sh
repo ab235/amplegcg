7fc484279842fdd38b38b4e11e3791c9958d4b72
root <root@LAPTOP-7VR2MFRI.localdomain>
2025-04-24 19:22:30 -0400

    .

--- add_reward.sh
+++ add_reward.sh
@@ -46,7 +46,7 @@ elif [ "$reward_mode" = "sequence" ]; then
   for top_dir in "${reward_dirs[@]}"
   do    
     find $top_dir -type f -name "*.jsonl" | while IFS= read -r file; do
-      python add_reward.py "path='$file'"   reward_lm=beaver-7b reward_lm.model_name=PKU-Alignment/beaver-7b-v1.0-cost only_prev_harms=[] "force_replace=$force_replace"
+      python add_reward.py "path='$file'"   reward_lm=beaver-7b  only_prev_harms=[] "force_replace=$force_replace"
       python add_reward.py "path='$file'" reward_lm=harmbench-13b only_prev_harms=["beaver"] "force_replace=$force_replace" batch_size=64
       # python add_reward.py "path='$file'" reward_lm=gpt4-0613 only_prev_harms=["beaver","harmbench"] "force_replace=$force_replace"
       echo $file
